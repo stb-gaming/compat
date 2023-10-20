74cd0675d9220e8876ce44b4b062bef2bf6bccf8
Tumble <tumble1999@users.noreply.github.com>
2023-10-20 22:02:20 +0100

    add browser version

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const getBrowserInfo = (function(){
+    var ua= navigator.userAgent;
+    var tem; 
+    var M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M.join(' ');
+});
+  
+  const browserInfo = getBrowserInfo();
+
+document.getElementById("browser").innerText = browserInfo
+
+
 
 function createCanvas() {
 	return document.createElement('canvas')
@@ -16,6 +38,7 @@ function checkWasm() {
 	}
 	return false;
 }
+
 
 const tests = {
 	js: true,
